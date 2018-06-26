e2a7d9eb0cbf63377036455f8e9917be712a4ffb
Mark Fitzpatrick <markfitz815@gmail.com>
2018-06-26 20:15:08 +0100

    working with filters and validation

---
+++ traversy/sec2/filters.php
@@ -0,0 +1,88 @@
+<?php 
+    /*
+    // Check for posted data
+    if(filter_has_var(INPUT_POST, 'data')){
+        echo 'Data Found';
+    }else{
+        echo 'No Data';
+    }
+   
+
+    
+
+    if(filter_has_var(INPUT_POST, 'data')){
+        $email = $_POST['data'];
+
+        // Remove illegal chars
+        $email = filter_var ($email, FILTER_SANITIZE_EMAIL);
+        echo $email . "<br>";
+
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            echo 'Email is valid';
+        }else{
+            echo 'Email is NOT valid';
+        }
+    }
+
+    $var = 23;
+
+    if(filter_var($var, FILTER_VALIDATE_INT)){
+        echo $var . ' is a number';
+    }else{
+        echo $var . ' is not a number';
+    }
+
+     $var = "33g343g2424";
+     var_dump(filter_var($var, FILTER_SANITIZE_NUMBER_INT));
+
+     $varBad = '<script>alert(1)</script>';
+     echo filter_var($varBad, FILTER_SANITIZE_SPECIAL_CHARS);
+
+     
+
+    $filters = [
+        "data" => FILTER_VALIDATE_EMAIL,
+        "data2" => [
+            "filter" => FILTER_VALIDATE_INT,
+            "options" => [
+                "min_range" => 1,
+                "max_range" => 100
+            ]
+        ]
+        ];
+
+    print_r(filter_input_array(INPUT_POST, $filters));
+
+    */
+
+    $arr = [
+        "name" => "mark fitz",
+        "age" => 27,
+        "email" => "mf@gmail.com" 
+    ];
+
+    $filters = [
+        "name" => [
+            "filter" => FILTER_CALLBACK,
+            "options" => "ucwords"
+        ],
+        "age" => [
+            "filter" => FILTER_VALIDATE_INT,
+            "options" => [
+                "min_range" => 1,
+                "max_range" => 120
+            ]
+        ],
+        "email" => FILTER_VALIDATE_EMAIL
+    ];
+
+    print_r(filter_var_array($arr, $filters));
+
+?>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="text" name="data">
+    <input type="text" name="data2">
+    <button type="submit">Submit</button>
+</form>
